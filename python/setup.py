8b1013323869fd0799e4ae5c644f4e43f49e2ece
Stephen <stephen@nowhere>
2018-07-07 00:00:00 +0000

    Set pypy description

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,20 @@
+import os
 from setuptools import setup, find_packages
 
 
+with open(os.path.join(os.path.dirname(__file__),
+                       '..', 'README.md')) as readme:
+    long_description = readme.read()
+
 setup(
     name='kameris-formats',
-    version='1.0.1',
+    version='1.0.3',
     description=('Reader and writer implementations for the file formats used '
                  'by kameris-backend.'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/stephensolis/kameris-formats/',
+
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
@@ -17,9 +26,9 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering'
     ],
+
     author='Stephen',
     author_email='stephsolis@gmail.com',
-    url='https://github.com/stephensolis/kameris-formats/',
     license='MIT',
     packages=find_packages(),
     install_requires=[
